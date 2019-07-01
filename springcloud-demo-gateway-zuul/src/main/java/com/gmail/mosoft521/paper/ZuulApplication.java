3f0df46149341e0af357fe71e00d6acf327b5cd0
Alvin Zhang <mosoft521@gmail.com>
2019-07-01 21:14:42 +0800

    gateway zuul

---
+++ springcloud-demo-gateway-zuul/src/main/java/com/gmail/mosoft521/paper/ZuulApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.mosoft521.paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
