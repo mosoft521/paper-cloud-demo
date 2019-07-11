3680768130de20a4581960a4251da00bd58f3fe1
Alvin Zhang <mosoft521@gmail.com>
2019-07-11 23:05:07 +0800

    springcloud-demo-boot-dict demo

---
+++ springcloud-demo-boot-dict/src/main/java/com/gmail/mosoft521/paper/SpringcloudDemoBootDictApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.mosoft521.paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudDemoBootDictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudDemoBootDictApplication.class, args);
+    }
+
+}
---
+++ springcloud-demo-boot-dict/src/test/java/com/gmail/mosoft521/paper/SpringcloudDemoBootDictApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.mosoft521.paper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudDemoBootDictApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
