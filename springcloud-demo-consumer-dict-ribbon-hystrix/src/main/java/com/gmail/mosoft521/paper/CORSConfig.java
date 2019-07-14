d3de403287d929b3e258f405b6adce761fc465a2
Alvin Zhang <mosoft521@gmail.com>
2019-07-14 19:11:07 +0800

    SpringBoot处理跨域请求（PUT,DELETE,GET,POST,OPTIONS）

--- springcloud-demo-consumer-dict-ribbon-hystrix/src/main/java/com/gmail/mosoft521/paper/CORSConfig.java
+++ springcloud-demo-consumer-dict-ribbon-hystrix/src/main/java/com/gmail/mosoft521/paper/CORSConfig.java
@@ -12,11 +12,15 @@ public class CORSConfig {
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**").allowedOrigins("*").allowedMethods("PUT", "DELETE", "GET", "POST")
-                    .allowedHeaders("*").exposedHeaders("access-control-allow-headers",
-                    "access-control-allow-methods", "access-control-allow-origin",
-                    "access-control-max-age", "X-Frame-Options")
-                    .allowCredentials(false).maxAge(3600);
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("PUT", "DELETE", "GET", "POST")
+                    .allowedHeaders("*")
+                    .exposedHeaders("access-control-allow-headers",
+                            "access-control-allow-methods", "access-control-allow-origin",
+                            "access-control-max-age", "X-Frame-Options")
+                    .allowCredentials(false)
+                    .maxAge(3600);
         }
     }
 }
